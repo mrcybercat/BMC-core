b061af02e0ab215451e60c72e7da13a010681826
Cat <mrcatmangreen@gmail.com>
2025-02-15 01:22:15 +0100

    fix#4: Solved issue with value-flipping by upcasting to int

--- src/main/java/multi/converter/algorithm/steps/convertions/ConvertRGBtoYUVStep.java
+++ src/main/java/multi/converter/algorithm/steps/convertions/ConvertRGBtoYUVStep.java
@@ -9,23 +9,36 @@ public class ConvertRGBtoYUVStep extends AlgorithmStep<YUVData, RGBData> {
     // TODO: Fix value flipping
     @Override
     public YUVData performAlgorithmStep(RGBData source) {
-        byte[][] luma       = new byte[source.getHeight()][source.getWidth()];
-        byte[][] chromaU    = new byte[source.getHeight()][source.getWidth()];
-        byte[][] chromaV    = new byte[source.getHeight()][source.getWidth()];
+        int[][] luma = new int[source.getHeight()][source.getWidth()];
+        int[][] chromaU = new int[source.getHeight()][source.getWidth()];
+        int[][] chromaV = new int[source.getHeight()][source.getWidth()];
 
         for (int y = 0; y < source.getHeight(); y++) {
             for (int x = 0; x < source.getWidth(); x++) {
-                luma[y][x]    = (byte) ( 0.257D * (double) source.getRed()  [y][x]
-                                       + 0.504D * (double) source.getGreen()[y][x]
-                                       + 0.098D * (double) source.getBlue() [y][x] + 16);
-                chromaU[y][x] = (byte) (-0.148D * (double) source.getRed()  [y][x]
-                                       - 0.291D * (double) source.getGreen()[y][x]
-                                       + 0.439D * (double) source.getBlue() [y][x] + 128);
-                chromaV[y][x] = (byte) ( 0.439D * (double) source.getRed()  [y][x]
-                                       - 0.368D * (double) source.getGreen()[y][x]
-                                       - 0.071D * (double) source.getBlue() [y][x] + 128);
+                int r = source.getRed()[y][x] & 0xFF;   // Convert byte to unsigned int
+                int g = source.getGreen()[y][x] & 0xFF;
+                int b = source.getBlue()[y][x] & 0xFF;
+
+                int yVal = (int) (0.257 * r + 0.504 * g + 0.098 * b + 16);
+                int uVal = (int) (-0.148 * r - 0.291 * g + 0.439 * b + 128);
+                int vVal = (int) (0.439 * r - 0.368 * g - 0.071 * b + 128);
+
+                luma[y][x] = Math.max(0, Math.min(255, yVal));
+                chromaU[y][x] = Math.max(0, Math.min(255, uVal));
+                chromaV[y][x] = Math.max(0, Math.min(255, vVal));
             }
         }
-        return YUVData.fromArrays(luma, chromaU, chromaV);
+
+        return YUVData.fromArrays(convertToByteArray(luma), convertToByteArray(chromaU), convertToByteArray(chromaV));
+    }
+    // Helper method to convert int array to byte array
+    private byte[][] convertToByteArray(int[][] source) {
+        byte[][] result = new byte[source.length][source[0].length];
+        for (int y = 0; y < source.length; y++) {
+            for (int x = 0; x < source[0].length; x++) {
+                result[y][x] = (byte) source[y][x];
+            }
+        }
+        return result;
     }
 }
--- src/main/java/multi/converter/algorithm/steps/convertions/ConvertYUVtoRGBStep.java
+++ src/main/java/multi/converter/algorithm/steps/convertions/ConvertYUVtoRGBStep.java
@@ -10,24 +10,41 @@ public class ConvertYUVtoRGBStep extends AlgorithmStep<RGBData, YUVData> {
     // TODO: Fix value flipping
     @Override
     public RGBData performAlgorithmStep(YUVData source) {
-        if(source.getHeight() != source.getHeightChromaU() || source.getWidth() != source.getWidthChromaV()){
+        if (source.getHeight() != source.getHeightChromaU() || source.getWidth() != source.getWidthChromaV()) {
             throw new UnableToPerformStepException("Cannot perform conversion if U or V channels were down scaled by unspecified factor");
         }
 
-        byte[][] red    = new byte[source.getHeight()][source.getWidth()];
-        byte[][] green  = new byte[source.getHeight()][source.getWidth()];
-        byte[][] blue   = new byte[source.getHeight()][source.getWidth()];
+        int[][] red   = new int[source.getHeight()][source.getWidth()];
+        int[][] green = new int[source.getHeight()][source.getWidth()];
+        int[][] blue  = new int[source.getHeight()][source.getWidth()];
 
         for (int y = 0; y < source.getHeight(); y++) {
             for (int x = 0; x < source.getWidth(); x++) {
-                byte luma    = (byte) (source.getLuma()[y][x]     - 16);
-                byte chromaU = (byte) (source.getChromaU()[y][x]  - 128);
-                byte chromaV = (byte) (source.getChromaV()[y][x]  - 128);
-                red[y][x]    = (byte) (1.164D * (double) luma                             + 1.596D * (double) chromaV);
-                green[y][x]  = (byte) (1.164D * (double) luma - 0.392D * (double) chromaU - 0.813D * (double) chromaV);
-                blue[y][x]   = (byte) (1.164D * (double) luma + 2.017D * (double) chromaU);
+                int luma    = (source.getLuma()[y][x] & 0xFF) - 16;      // Convert byte to unsigned int
+                int chromaU = (source.getChromaU()[y][x] & 0xFF) - 128;  // Convert byte to unsigned int
+                int chromaV = (source.getChromaV()[y][x] & 0xFF) - 128;
+
+                int r = (int) (1.164 * luma + 1.596 * chromaV);
+                int g = (int) (1.164 * luma - 0.392 * chromaU - 0.813 * chromaV);
+                int b = (int) (1.164 * luma + 2.017 * chromaU);
+
+                red[y][x]   = Math.max(0, Math.min(255, r));
+                green[y][x] = Math.max(0, Math.min(255, g));
+                blue[y][x]  = Math.max(0, Math.min(255, b));
             }
         }
-        return RGBData.fromArrays(red, green, blue);
+
+        return RGBData.fromArrays(convertToByteArray(red), convertToByteArray(green), convertToByteArray(blue));
+    }
+
+    // Helper method to convert int array to byte array
+    private byte[][] convertToByteArray(int[][] source) {
+        byte[][] result = new byte[source.length][source[0].length];
+        for (int y = 0; y < source.length; y++) {
+            for (int x = 0; x < source[0].length; x++) {
+                result[y][x] = (byte) source[y][x];
+            }
+        }
+        return result;
     }
 }
